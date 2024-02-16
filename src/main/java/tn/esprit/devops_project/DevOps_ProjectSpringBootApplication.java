0ec24ab1d51afc80e8d69dd054b85f99934a4087
Mohamed-Mi20 <miaoui.mohamed12@gmail.com>
2024-02-16 10:17:47 +0100

    first c

--- src/main/java/tn/esprit/devops_project/DevOps_ProjectSpringBootApplication.java
+++ src/main/java/tn/esprit/devops_project/DevOps_ProjectSpringBootApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DevOps_ProjectSpringBootApplication {
-
+    //blabla
     public static void main(String[] args) {
         SpringApplication.run(DevOps_ProjectSpringBootApplication.class, args);
     }
