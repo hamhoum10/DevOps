29d74ff6ab8c893ce8d9733f7a01815c1426c808
Mohamed-Mi20 <miaoui.mohamed12@gmail.com>
2024-02-29 23:55:11 +0100

    .

--- src/test/java/tn/esprit/devops_project/services/OperatorServiceImplTest.java
+++ src/test/java/tn/esprit/devops_project/services/OperatorServiceImplTest.java
@@ -13,7 +13,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
-public class OperatorServiceImplTest {
+class OperatorServiceImplTest {
 
     @Mock
     private OperatorRepository operatorRepository;
