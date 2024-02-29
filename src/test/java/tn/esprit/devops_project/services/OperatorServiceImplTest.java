9f49a900b1c9098fa4f29949f6575d6b0fc5a134
Mohamed-Mi20 <miaoui.mohamed12@gmail.com>
2024-02-29 22:49:54 +0100

    plugin

---
+++ src/test/java/tn/esprit/devops_project/services/OperatorServiceImplTest.java
@@ -0,0 +1,27 @@
+package tn.esprit.devops_project.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class OperatorServiceImplTest {
+
+    @Test
+    void retrieveAllOperators() {
+    }
+
+    @Test
+    void addOperator() {
+    }
+
+    @Test
+    void deleteOperator() {
+    }
+
+    @Test
+    void updateOperator() {
+    }
+
+    @Test
+    void retrieveOperator() {
+    }
+}
