57e02b3a985c6afde41a054b883ded06c4720ce7
MohamedAmineNasri <mohamedamine.nasri@esprit.tn>
2024-02-29 22:51:55 +0100

    jacoco

---
+++ src/test/java/tn/esprit/devops_project/services/InvoiceServiceImplTest.java
@@ -0,0 +1,32 @@
+package tn.esprit.devops_project.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InvoiceServiceImplTest {
+
+    @Test
+    void retrieveAllInvoices() {
+    }
+
+    @Test
+    void cancelInvoice() {
+    }
+
+    @Test
+    void retrieveInvoice() {
+    }
+
+    @Test
+    void getInvoicesBySupplier() {
+    }
+
+    @Test
+    void assignOperatorToInvoice() {
+    }
+
+    @Test
+    void getTotalAmountInvoiceBetweenDates() {
+    }
+}
