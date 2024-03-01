89dad17c955dd5ae28176e8a3b62c60773320415
MohamedAmineNasri <mohamedamine.nasri@esprit.tn>
2024-03-01 11:46:39 +0100

    coverage

--- src/main/java/tn/esprit/devops_project/DevOpsProjectSpringBootApplication.java
+++ src/main/java/tn/esprit/devops_project/DevOpsProjectSpringBootApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DevOps_ProjectSpringBootApplication {
+public class DevOpsProjectSpringBootApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DevOps_ProjectSpringBootApplication.class, args);
+        SpringApplication.run(DevOpsProjectSpringBootApplication.class, args);
     }
 
 }
